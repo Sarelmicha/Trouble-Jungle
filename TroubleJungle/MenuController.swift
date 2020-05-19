503b9cc185850a25edec126facccb15f88ee909a
Sarel Micha <sarelmicha93@gmail.com>
2020-05-19 13:02:14 +0100

    *Menu Screen Had been added *Game Over Screen has been added

---
+++ TroubleJungle/MenuController.swift
@@ -0,0 +1,55 @@
+//
+//  MenuController.swift
+//  TroubleJungle
+//
+//  Created by user167401 on 5/19/20.
+//  Copyright © 2020 Sarel Micha. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+    
+    
+    @IBOutlet weak var menu_BTN_easy: UIButton!
+    @IBOutlet weak var menu_BTN_hard: UIButton!
+    @IBOutlet weak var menu_BTN_scores: UIButton!
+    
+    let EASY_MODE = "Easy"
+    let HARD_MODE = "Hard"
+    
+    var difficult : String?;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onDifficultButtonPressed(_ sender: UIButton) {
+        
+        difficult = sender.titleLabel!.text
+        
+        self.performSegue(withIdentifier: "goToGamePage", sender: self)
+        
+    }
+    
+    
+    //MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "goToGamePage"){
+            let gamePage = segue.destination as! GameController
+             //Set the game parameters
+            if(difficult == EASY_MODE){
+        
+                gamePage.numOfRows = 3;
+                gamePage.numOfCardsPerRow = 4;
+            } else if (difficult == HARD_MODE){
+                
+                gamePage.numOfRows = 4;
+                gamePage.numOfCardsPerRow = 4;
+            }
+        }
+    }
+}
