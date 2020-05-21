696992ee81084e02b5b42cdb6476259bdbeb3ed7
Sarel Micha <sarelmicha93@gmail.com>
2020-05-21 14:23:38 +0100

    *Score page has been added only draft

---
+++ TroubleJungle/ScoresController.swift
@@ -0,0 +1,30 @@
+//
+//  ScoresController.swift
+//  TroubleJungle
+//
+//  Created by user167401 on 5/21/20.
+//  Copyright © 2020 Sarel Micha. All rights reserved.
+//
+
+import UIKit
+
+class ScoresController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
