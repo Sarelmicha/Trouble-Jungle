b78687e87b8ae6870ee1ca825460ee6237d5fe5f
Sarel Micha <sarelmicha93@gmail.com>
2020-05-21 14:27:30 +0100

    *Fix bugs

--- TroubleJungle/ScoresController.swift
+++ TroubleJungle/ScoresController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ScoresController: UIViewController {
+class ScoresController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           <#code#>
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           <#code#>
+       }
     
 
     /*
