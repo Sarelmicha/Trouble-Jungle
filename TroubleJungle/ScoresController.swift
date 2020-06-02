488f31e27a41eb3688240aaebac81cd8edb5c649
Sarel Micha <sarelmicha93@gmail.com>
2020-06-02 14:39:04 +0100

    * FINISHED

--- TroubleJungle/ScoresController.swift
+++ TroubleJungle/ScoresController.swift
@@ -73,11 +73,10 @@ class ScoresController: UIViewController, UITableViewDelegate, UITableViewDataSo
     
     func showAnotation(index : Int){
         
-       
+
         newCamera = MKMapCamera(lookingAtCenter: CLLocationCoordinate2D(latitude: highScores[index].location.lat!, longitude: highScores[index].location.lng!), fromDistance: 500.0, pitch: 90.0, heading: 180.0)
         self.scores_MAPVIEW_map.setCamera(newCamera, animated: true)
-        
-
+    
     }
     
     func setupList(){
@@ -107,12 +106,8 @@ class ScoresController: UIViewController, UITableViewDelegate, UITableViewDataSo
     }
     
      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("here bitchhhhh")
         showAnotation(index: indexPath.row)
     }
-    
-    
-
     
 }
 
